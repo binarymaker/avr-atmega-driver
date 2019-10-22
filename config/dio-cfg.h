db1d8dd6a677da668ae4be7f3a45c74ea2039d8a
binarymaker <importfunfromcode@gmail.com>
2019-10-22 14:26:00 +0530

    dio file added

---
+++ config/dio-cfg.h
@@ -0,0 +1,40 @@
+/**\cond
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  \endcond*/
+
+#ifndef DIO_715e6b5c_f4a5_11e9_b4bc_705a0f25cb51
+#define DIO_715e6b5c_f4a5_11e9_b4bc_705a0f25cb51
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DIO_715e6b5c_f4a5_11e9_b4bc_705a0f25cb51 */
+
