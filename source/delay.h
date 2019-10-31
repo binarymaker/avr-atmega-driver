4caf43b22782580b071332ab4043745b68c6e039
binarymaker <importfunfromcode@gmail.com>
2019-10-31 09:57:49 +0530

    template files added

---
+++ source/delay.h
@@ -0,0 +1,47 @@
+/**\cond
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  \endcond*/
+
+#ifndef DELAY_1894e694_fb96_11e9_b847_0c5b8f279a64
+#define DELAY_1894e694_fb96_11e9_b847_0c5b8f279a64
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * \brief Source file version tag
+ *        
+ *        version info: [15:8] main [7:0] beta
+ */
+#define __DELAY_VERSION      (0x0001u)
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DELAY_1894e694_fb96_11e9_b847_0c5b8f279a64 */
+
---
+++ source/gpio.h
@@ -0,0 +1,47 @@
+/**\cond
+  ******************************************************************************
+  * ______  _                             ___  ___        _               
+  * | ___ \(_)                            |  \/  |       | |              
+  * | |_/ / _  _ __    __ _  _ __  _   _  | .  . |  __ _ | | __ ___  _ __ 
+  * | ___ \| || '_ \  / _` || '__|| | | | | |\/| | / _` || |/ // _ \| '__|
+  * | |_/ /| || | | || (_| || |   | |_| | | |  | || (_| ||   <|  __/| |   
+  * \____/ |_||_| |_| \__,_||_|    \__, | \_|  |_/ \__,_||_|\_\\___||_|   
+  *                                 __/ |                                 
+  *                                |___/                                  
+  *                                                                       
+  * Copyright (C) 2019 Binary Maker - All Rights Reserved
+  *
+  * This program and the accompanying materials are made available
+  * under the terms described in the LICENSE file which accompanies
+  * this distribution.
+  * Written by Binary Maker <https://github.com/binarymaker>
+  ******************************************************************************
+  \endcond*/
+
+#ifndef GPIO_1ee7051c_fb96_11e9_b0d7_0c5b8f279a64
+#define GPIO_1ee7051c_fb96_11e9_b0d7_0c5b8f279a64
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/**
+ * \brief Source file version tag
+ *        
+ *        version info: [15:8] main [7:0] beta
+ */
+#define __GPIO_VERSION      (0x0001u)
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GPIO_1ee7051c_fb96_11e9_b0d7_0c5b8f279a64 */
+
